49ad551c8ebaf1130be4d228f339c5e95fe4ec15
defaultlister <defaultlister@gmail.com>
2025-07-02 11:34:49 +0300

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,13 +1,23 @@
-wrap([$class: 'ServerBuildWrapper',
-      secrets: [
-        [
-          id: config.id,
-          baseUrl: '',
-          credentialId: env.SECRET_SERVER_CRED,
-          mappings: [
-            [field: 'Private Key', environmentVariable: 'privatekey'],
-            [field: 'Private Key Passphrase', environmentVariable: 'passphrase']
+def call(Map config = [:], Closure body = null) {
+    wrap([$class: 'ServerBuildWrapper',
+          secrets: [
+            [
+              id: config.id,
+              baseUrl: '',
+              credentialId: env.SECRET_SERVER_CRED,
+              mappings: [
+                [field: 'Username', environmentVariable: 'username'],
+                [field: 'Password', environmentVariable: 'password'],
+                [field: 'SecretFile', environmentVariable: 'secretfile'],
+                [field: 'Private Key', environmentVariable: 'privatekey'],
+                [field: 'Public Key', environmentVariable: 'publickey'],
+                [field: 'Private Key Passphrase', environmentVariable: 'passphrase']
+              ]
+            ]
           ]
-        ]
-      ]
-])
+    ]) {
+        if (body != null) {
+            body()
+        }
+    }
+}
