74c1b5f338c81c846f2fdbf21c3f4a948683269b
defaultlister <defaultlister@gmail.com>
2025-07-02 11:28:09 +0300

    test only ssh

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,19 +1,13 @@
-def call(Map config = [:], Closure body = null) {
-    wrap([$class: 'ServerBuildWrapper',
-          secrets: [
-            [
-              id: config.id,
-              baseUrl: '',
-              credentialId: env.SECRET_SERVER_CRED,
-              mappings: [
-                [field: 'Private Key', environmentVariable: 'privatekey'],
-                [field: 'Private Key Passphrase', environmentVariable: 'passphrase']
-              ]
-            ]
+wrap([$class: 'ServerBuildWrapper',
+      secrets: [
+        [
+          id: config.id,
+          baseUrl: '',
+          credentialId: env.SECRET_SERVER_CRED,
+          mappings: [
+            [field: 'Private Key', environmentVariable: 'privatekey'],
+            [field: 'Private Key Passphrase', environmentVariable: 'passphrase']
           ]
-    ]) {
-        if (body != null) {
-            body()
-        }
-    }
-}
+        ]
+      ]
+])
