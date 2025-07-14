9191b74773608beb9ca59c325e0d42056f7efd00
defaultlister <defaultlister@gmail.com>
2025-07-14 12:32:29 +0300

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -7,10 +7,10 @@ def call(Map config = [:], Closure body = null) {
               baseUrl: '',
               credentialId: env.SECRET_SERVER_CRED,
               mappings: [
-                [field: 'Username', environmentVariable: "${prefix}_USERNAME"],
-                [field: 'Password', environmentVariable: "${prefix}_PASSWORD"],
-                [field: 'SecretFile', environmentVariable: "${prefix}_FILE"],
-                [field: 'itemValue', environmentVariable: "${prefix}_ITEM"]
+                [field: 'Username', environmentVariable: "${prefix}_username"],
+                [field: 'Password', environmentVariable: "${prefix}_password"],
+                [field: 'SecretFile', environmentVariable: "${prefix}_file"],
+                [field: 'itemValue', environmentVariable: "${prefix}_item"]
               ]
           ]]
     ]) {
