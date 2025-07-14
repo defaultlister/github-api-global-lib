5c02d72fe762102b6f2736bd6f01f401d14f6356
defaultlister <defaultlister@gmail.com>
2025-07-14 13:51:11 +0300

    added prefix for multipass usage

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,21 +1,21 @@
 def call(Map config = [:], Closure body = null) {
+    def prefix = config.prefix ?: "SECRET"
+
     wrap([$class: 'ServerBuildWrapper',
-          secrets: [
-            [
+          secrets: [[
               id: config.id,
               baseUrl: '',
               credentialId: env.SECRET_SERVER_CRED,
               mappings: [
-                [field: 'Username', environmentVariable: 'username'],
-                [field: 'Password', environmentVariable: 'password'],
-                [field: 'SecretFile', environmentVariable: 'secretfile'],
-                [field: 'PrivateKey', environmentVariable: 'privatekey'],
-                [field: 'PublicKey', environmentVariable: 'publickey'],
-                [field: 'itemValue', environmentVariable: 'itemValue'],
-                [field: 'PrivateKeyPassphrase', environmentVariable: 'passphrase']
+                [field: 'Username', environmentVariable: "${prefix}_username"],
+                [field: 'Password', environmentVariable: "${prefix}_password"],
+                [field: 'SecretFile', environmentVariable: "${prefix}_secretfile"],
+                [field: 'PrivateKey', environmentVariable: "${prefix}_privatekey"],
+                [field: 'PublicKey', environmentVariable: "${prefix}_publickey"],
+                [field: 'itemValue', environmentVariable: "${prefix}_itemValue"],
+                [field: 'PrivateKeyPassphrase', environmentVariable: "${prefix}_passphrase"]
               ]
-            ]
-          ]
+          ]]
     ]) {
         if (body != null) {
             body()
