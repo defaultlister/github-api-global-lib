5fdbce7bf1c272969cf380d41c9ec3df8b3779a3
defaultlister <defaultlister@gmail.com>
2025-07-21 16:33:24 +0300

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -8,24 +8,25 @@ def call(List<Map> secretsList = [], Closure body) {
             error("Each secret must have 'id' and 'prefix' defined.")
         }
 
-        def vaultUrl = entry.get('vaultUrl', 'https://your.delinea.secretserver')
         def credentialId = entry.get('credentialId', 'delinea_jenkins')
-        def fieldSlugs = entry.get('fieldSlugs', ['Username', 'Password'])
+        def fields = entry.get('fields', ['Username', 'Password']) // Plugin does NOT support 'fieldSlugs'
 
-        def mappings = fieldSlugs.collect { field ->
-            [field: field, environmentVariable: "${entry.prefix}_${field.toUpperCase()}"]
+        def mappings = fields.collect { field ->
+            [
+                field               : field,
+                environmentVariable: "${entry.prefix}_${field.toUpperCase()}"
+            ]
         }
 
         return [
-            id          : entry.id,
-            vaultUrl    : vaultUrl,
-            credentialId: credentialId,
-            fieldSlugs  : fieldSlugs,
-            mappings    : mappings
+            $class       : 'com.delinea.secrets.jenkins.wrapper.cred.ServerSecret',
+            id           : entry.id,
+            credentialId : credentialId,
+            mappings     : mappings
         ]
     }
 
-    wrap([$class: 'ServerBuildWrapper', secrets: secrets]) {
+    wrap([$class: 'com.delinea.secrets.jenkins.wrapper.cred.ServerBuildWrapper', secrets: secrets]) {
         body()
     }
 }
